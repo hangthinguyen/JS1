1471e6ad97a71bae589d5c5e1498a3bc18f20077
Hang Nguyen <thuhang.nguyen0101@gmail.com>
2022-04-06 16:05:09 -0700

    complete adding to to-do list via jquery

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-
+console.log($)
 
 document.querySelector('form').addEventListener('submit', function (e) {
     //prevent the normal submission of the form
     e.preventDefault();
 
+    //******************************************************** JQUERY
+
+    const input = $('#todo-input');
+
     /**
      * Create a new list item.
      */
-    const newTodoListItem = document.createElement('p');
-    const input = document.getElementById('todo-input');
-    const value = document.createTextNode(input.value);
-    newTodoListItem.appendChild(value)
+    const newTodoListItem = `<p>${input.val()}</p>`;
 
     /**
      * Add new item from previous step to the todo list.
      */
-    const list = document.getElementById('todo-list');
-    list.appendChild(newTodoListItem)
+    $('#todo-list').append(newTodoListItem);
 
     /**
      * Clear input value afteer successfully adding the todo item to the todo list.
      */
-    input.value = '';
+    input.val('');
+
+        // ****************************************************** VANILLA JS
+
+    /**
+     * Create a new list item.
+     */
+    // const newTodoListItem = document.createElement('p');
+    // const input = document.getElementById('todo-input');
+    // const value = document.createTextNode(input.value);
+    // newTodoListItem.appendChild(value)
+
+    /**
+     * Add new item from previous step to the todo list.
+     */
+    // const list = document.getElementById('todo-list');
+    // list.appendChild(newTodoListItem)
+
+    /**
+     * Clear input value afteer successfully adding the todo item to the todo list.
+     */
+    // input.value = '';
 });
